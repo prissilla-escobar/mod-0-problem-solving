93bfadcc537194e8e7aec5bbba9b427990f89784
Prissilla Escobar <prissilla988@gmail.com>
2023-04-21 12:37:36 -0600

    Initial commit

---
+++ challenge1.js
@@ -0,0 +1,7 @@
+/* Given an array of strings, print only the strings that have exactly 4 characters. */
+
+/* I am going to name and make an array of strings and write code that prints the elements
+that have exactly 4 characters.
+I need to make an array, a for statement and look up a method to count characters in
+order to include in my print code. */
+
