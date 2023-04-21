8b0df2b70d70ef4fb0bd0bee7fc31524bd9b324f
Prissilla Escobar <prissilla988@gmail.com>
2023-04-21 12:47:47 -0600

    Intial commit

---
+++ 3challenge.js
@@ -0,0 +1,5 @@
+/*Write a method or function that removes all instances of the letter s in a string.
+The method or function should accept a string as an
+argument and return the same string with every instance of the letter s removed. */
+
+/* I need the output to remove */
