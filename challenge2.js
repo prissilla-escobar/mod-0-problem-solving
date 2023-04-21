9a4f855351911ea03ff58f0f920c8f88e9ea11d6
Prissilla Escobar <prissilla988@gmail.com>
2023-04-21 12:33:08 -0600

    add pseudocode and my explanation

--- challenge2.js
+++ challenge2.js
@@ -1,6 +1,10 @@
 /* Start with an array of strings with a mix of uppercase and lowercase letters.
 Print every word in all lowercase letters*/
 
+/* I want all string elements to print in all lowercase.
+I need to make an array and a for statement.
+I am going to look up a string method to make all letters lowercase */
+
 var siblings = ["PriSilla", "frankie", "KRISTINA", "AlexANDRA"]
     for (var i = 0; i < siblings.length; i++) {
         console.log(siblings[i].toLowerCase())
