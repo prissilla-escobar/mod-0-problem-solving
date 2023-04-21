6a3316d817ab4eee43e6ce5efc12b6feff1a8abb
Prissilla Escobar <prissilla988@gmail.com>
2023-04-21 12:45:03 -0600

    create for statement and conditional to print elements with exactly 4 characters

--- challenge1.js
+++ challenge1.js
@@ -2,6 +2,13 @@
 
 /* I am going to name and make an array of strings and write code that prints the elements
 that have exactly 4 characters.
-I need to make an array, a for statement and look up a method to count characters in
+I need to make an array, a for statement, a conditional and look up a method to count
+characters in
 order to include in my print code. */
 
+var objects = [ "car", "phone", "tree", "wire", "sofa", "mirror"]
+for (var i = 0; i < objects.length; i++) {
+    if (objects[i].length === 4){
+        console.log(objects[i])
+    }
+}
