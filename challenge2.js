60838876f58a4c3b5e7b30a53e9a06260dc7290e
Prissilla Escobar <prissilla988@gmail.com>
2023-04-21 12:29:04 -0600

    code for statement and print code to print all elements in the array in lowercase

--- challenge2.js
+++ challenge2.js
@@ -1,3 +1,7 @@
 /* Start with an array of strings with a mix of uppercase and lowercase letters.
 Print every word in all lowercase letters*/
 
+var siblings = ["PriSilla", "frankie", "KRISTINA", "AlexANDRA"]
+    for (var i = 0; i < siblings.length; i++) {
+        console.log(siblings[i].toLowerCase())
+    }
